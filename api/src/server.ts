188be8dae653159436557f6116f9bc749ad366c1
Andre Oliveira <andreoliveirar2d2@gmail.com>
2020-06-03 14:41:42 -0400

    (new feature) moving routes to a separated files

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,33 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
+
 app.use(express.json());
-
-const users = [
-  'Andre',
-  'Oliveira',
-  'Teste',
-];
-
-app.get('/users', (req, res) => { 
-  const search = String(req.query.search);
-  const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
-
-  return res.json(filteredUsers);
-});
-
-app.get('/users/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const user = users[id];
-
-  return res.json(user);
-});
-
-app.post('/users', (req, res) => {
-  const data = req.body;
-  const user = { name: data.name, email: data.email };
-
-  return res.json(user);
-});
+app.use(routes);
 
 app.listen(3333);
