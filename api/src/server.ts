529dfb065a05479260eb41dc1a6f48fe3cedc5b1
Andre Oliveira <andreoliveirar2d2@gmail.com>
2020-06-03 13:39:20 -0400

    (new feature) getting user from array using index

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,8 +2,21 @@ import express from 'express';
 
 const app = express();
 
+const users = [
+  'Andre',
+  'Oliveira',
+  'Teste',
+];
+
 app.get('/users', (req, res) => { 
-  return res.json({ ok: true });
+  return res.json(users);
+});
+
+app.get('/users/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const user = users[id];
+
+  return res.json(user);
 });
 
 app.post('/users', (req, res) => {
