feaef76a9756cd3e0b065611f40e91dede8f4d1c
Andre Oliveira <andreoliveirar2d2@gmail.com>
2020-06-04 19:34:11 -0400

    (new feature) separating the routes methods to controllers

---
+++ api/src/controllers/PointsController.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from 'express';
+import knex from '../database/connection';
+
+class PointsController {
+  async create (req: Request, res: Response) {
+    const { name, email, whatsapp, longitude, latitude, uf, city, items } = req.body;
+    const trx = await knex.transaction();
+    const point = {
+      image: 'fake-image',
+      name,
+      email,
+      whatsapp,
+      longitude,
+      latitude,
+      uf,
+      city
+    };
+
+    const inserted_ids = await trx('points').insert(point);
+
+    const point_id = inserted_ids[0];
+    const pointItems = items.map((item_id: Number) => {
+      return {
+        item_id,
+        point_id,
+      };
+    });
+
+    await trx('point_items').insert(pointItems);
+
+    return res.json({
+      id: point_id,
+      ...point
+    });
+  };
+};
+
+export default PointsController;
