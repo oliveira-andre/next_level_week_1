d810d77c5477440ee67f7179243d82eeb6b3a651
Andre Oliveira <desmonplays@gmail.com>
2020-06-04 18:58:38 -0400

    (new feature) returning items

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import knex from './database/connection';
 const routes = express.Router();
 
-routes.get('/', (req, res) => { 
-  return res.json({ message: 'Hello World' });
+routes.get('/items', async (req, res) => { 
+  const items = await knex('items').select('*');
+
+  return res.json(items);
 });
 
 export default routes;
