d8e16da957ed59ae44a54551fda46e80fe3119fe
Andre Oliveira <desmonplays@gmail.com>
2020-06-04 19:53:07 -0400

    (new feature) getting points based on queryies

--- api/src/controllers/PointsController.ts
+++ api/src/controllers/PointsController.ts
@@ -2,6 +2,20 @@ import { Request, Response } from 'express';
 import knex from '../database/connection';
 
 class PointsController {
+  async index (req: Request, res: Response) {
+    const { city, uf, items } = req.query;
+    const parsedItems = String(items).split(',').map(item => Number(item.trim()));
+    const points = await knex('points')
+      .join('point_items', 'points.id', '=', 'point_items.point_id')
+      .whereIn('point_items.item_id', parsedItems)
+      .where('city', String(city))
+      .where('uf', String(uf))
+      .distinct()
+      .select('points.*');
+
+    return res.json(points);
+  };
+
   async create (req: Request, res: Response) {
     const { name, email, whatsapp, longitude, latitude, uf, city, items } = req.body;
     const trx = await knex.transaction();
@@ -27,6 +41,7 @@ class PointsController {
     });
 
     await trx('point_items').insert(pointItems);
+    await trx.commit();
 
     return res.json({
       id: point_id,
--- api/src/routes.ts
+++ api/src/routes.ts
@@ -10,6 +10,7 @@ const routes = express.Router();
 
 routes.get('/items', itemsController.index);
 
+routes.get('/points', pointsController.index);
 routes.post('/points', pointsController.create);
 routes.get('/points/:id', pointsController.show);
 
