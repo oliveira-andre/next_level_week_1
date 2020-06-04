6cfbd83c8c764636509cb5b2daa81421c9a123b0
Andre Oliveira <desmonplays@gmail.com>
2020-06-04 19:34:11 -0400

    (new feature) separating the routes methods to controllers

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,46 +1,14 @@
 import express from 'express';
-import knex from './database/connection';
+
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
+
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
 const routes = express.Router();
 
-routes.get('/items', async (req, res) => { 
-  const items = await knex('items').select('*');
-  const serializedItems = items.map(item => {
-    return {
-      id: item.id,
-      title: item.title,
-      image_url: `http://localhost:3333/uploads/${item.image}`,
-    }; 
-  });
-
-  return res.json(serializedItems);
-});
-
-routes.post('/points', async (req, res) => {
-  const { name, email, whatsapp, longitude, latitude, uf, city, items } = req.body;
-  const trx = await knex.transaction();
-
-  const inserted_ids = await trx('points').insert({
-    image: 'fake-image',
-    name,
-    email,
-    whatsapp,
-    longitude,
-    latitude,
-    uf,
-    city
-  });
-
-  const point_id = inserted_ids[0];
-  const pointItems = items.map((item_id: Number) => {
-    return {
-      item_id,
-      point_id,
-    };
-  });
-
-  await trx('point_items').insert(pointItems);
-
-  return res.json({ success: true });
-});
+routes.get('/items', itemsController.index);
+routes.post('/points', pointsController.create);
 
 export default routes;
