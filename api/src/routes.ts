86f37d8cd6487a32a6d71386e65e834b40dbf7e9
Andre Oliveira <andreoliveirar2d2@gmail.com>
2020-06-04 19:38:54 -0400

    (new feature) show point

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -9,6 +9,8 @@ const itemsController = new ItemsController();
 const routes = express.Router();
 
 routes.get('/items', itemsController.index);
+
 routes.post('/points', pointsController.create);
+routes.get('/points/:id', pointsController.show);
 
 export default routes;
