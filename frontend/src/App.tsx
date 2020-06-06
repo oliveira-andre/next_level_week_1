e31c8195acb06b6e7784825a22a49a79414a238f
Andre Oliveira <andreoliveirar2d2@gmail.com>
2020-06-06 16:20:29 -0400

    (new feature) importing header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
+    <>
+    <Header />
     <h1>Hello World</h1>
+    </>
   );
 }
 
