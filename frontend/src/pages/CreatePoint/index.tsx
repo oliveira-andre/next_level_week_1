3d893d665ba8fa6c1818daf431c39716085606a8
Andre Oliveira <desmonplays@gmail.com>
2020-06-07 14:32:28 -0400

    (new feature) html mock code working

--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -16,8 +16,96 @@ const CreatePoint = () => {
           Voltar para home
         </Link>
       </header>
-    </div>
-  );
+
+      <form>
+        <h1>Cadastro do <br /> ponto de coleta</h1>
+
+        <fieldset>
+          <legend>
+            <h2>Dados</h2>
+          </legend>
+
+            <div className="field">
+              <label htmlFor="name">Nome da entidade</label>
+              <input type="text" id="name" name="name" />
+            </div>
+
+            <div className="field-group">            
+              <div className="field">
+                <label htmlFor="email">E-mail</label>
+                <input type="text" id="email" name="email" />
+              </div>
+
+              <div className="field">
+                <label htmlFor="whatsapp">WhatsApp</label>
+                <input type="text" id="whatsapp" name="whatsapp" />
+              </div>
+            </div>
+          </fieldset>
+
+
+          <fieldset>
+            <legend>
+              <h2>Endereço</h2>
+              <span>Selecione um endereço no mapa</span>
+            </legend>
+
+            <div className="field-group">
+              <div className="field">
+                <label htmlFor="uf">Estado (UF)</label>
+                <select id="uf" name="uf">
+                  <option value="0">Selecione uma UF</option>
+                </select>
+              </div>
+
+              <div className="field">
+                <label htmlFor="city">Cidade</label>
+                <select id="city" name="city">
+                  <option value="0">Selecione uma Cidade</option>
+                </select>
+              </div>
+            </div>
+          </fieldset>
+
+
+          <fieldset>
+            <legend>
+              <h2>Ítens de coleta</h2>
+              <span>Selecione um ou mais ítens abaixo</span>
+            </legend>
+
+            <ul className="items-grid">
+              <li>
+                <img src="http://localhost:3333/uploads/lampadas.svg" alt="Lâmpadas" />
+                <span>Lâmpadas</span>
+              </li>
+              <li>
+                <img src="http://localhost:3333/uploads/lampadas.svg" alt="Lâmpadas" />
+                <span>Lâmpadas</span>
+              </li>
+              <li>
+                <img src="http://localhost:3333/uploads/lampadas.svg" alt="Lâmpadas" />
+                <span>Lâmpadas</span>
+              </li>
+              <li>
+                <img src="http://localhost:3333/uploads/lampadas.svg" alt="Lâmpadas" />
+                <span>Lâmpadas</span>
+              </li>
+              <li>
+                <img src="http://localhost:3333/uploads/lampadas.svg" alt="Lâmpadas" />
+                <span>Lâmpadas</span>
+              </li>
+              <li>
+                <img src="http://localhost:3333/uploads/lampadas.svg" alt="Lâmpadas" />
+                <span>Lâmpadas</span>
+              </li>
+            </ul>
+          </fieldset>
+
+          <button type="submit">Cadastrar ponto de coleta</button>
+        </form>
+      </div>
+    );
 };
 
 export default CreatePoint;
