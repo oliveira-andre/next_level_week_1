c300e9dccd63e1f3cf111c43e02a9ba26b0d3572
Andre Oliveira <andreoliveirar2d2@gmail.com>
2020-06-07 14:51:55 -0400

    (new feature) integrate map on form

--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Map, TileLayer, Marker } from 'react-leaflet';
 
 import './styles.css';
 import logo from '../../assets/logo.svg';
@@ -49,6 +50,12 @@ const CreatePoint = () => {
               <h2>Endereço</h2>
               <span>Selecione um endereço no mapa</span>
             </legend>
+
+            <Map center={[-8.7982609, -63.8995109]} zoom={15}>
+              <TileLayer attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+              <Marker position={[-8.7982609, -63.8995109]} />
+            </Map>
 
             <div className="field-group">
               <div className="field">
